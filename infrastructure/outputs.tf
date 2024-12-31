fb47e132a4f88673d878a3f25e06551f2ec87093
Mociofleaca Alin <alin.stefan.mociofleaca@gmail.com>
2024-12-31 14:56:02 +0200

    test new infra

---
+++ infrastructure/outputs.tf
@@ -0,0 +1,7 @@
+output "grafana_agent_public_ip" {
+  value = cloudscale_server.grafana_agent.public_ip
+}
+
+output "loadbalancer_service_public_ip" {
+  value = kubernetes_service.loadbalancer_service.status[0].load_balancer[0].ingress[0].ip
+}
